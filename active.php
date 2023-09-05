1a0534f2cda9970cdd4afe768a5860d7f3868099
olegkhorobriv <59508755+olegkhorobriv@users.noreply.github.com>
2023-09-05 09:58:26 +0300

    Update active.php

--- active.php
+++ active.php
@@ -1,9 +1,15 @@
 <?php
 require_once 'function/function_db.php';
-$selectedAction = $_POST['selectedAction'];
+//$selectedAction = $_POST['selectedAction'];
+//
+//$query = "SELECT * FROM `user` WHERE `status` = 'true'";
+//$result =  select($query);
+//foreach ($result as $res){
+//    require 'box.php';
+//}
+$selectedIds = $_POST['selectedIds'];
 
-$query = "SELECT * FROM `user` WHERE `status` = 'true'";
-$result =  select($query);
-foreach ($result as $res){
-    require 'box.php';
+for($i = 0; $i < count($selectedIds); $i++){
+    $query = "UPDATE `user` SET `status` = 'true' WHERE `user`.`id` = $selectedIds[$i]";
+    execQuery($query);
 }
