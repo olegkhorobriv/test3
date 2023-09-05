d4b0f2b9c58fc93606b1f599f954c7e759431475
olegkhorobriv <59508755+olegkhorobriv@users.noreply.github.com>
2023-09-05 09:58:57 +0300

    Update not_active.php

--- not_active.php
+++ not_active.php
@@ -1,9 +1,17 @@
 <?php
 require_once 'function/function_db.php';
-$selectedAction = $_POST['selectedAction'];
+//$selectedAction = $_POST['selectedAction'];
+//
+//$query = "SELECT * FROM `user` WHERE `status` = 'false'";
+//$result =  select($query);
+//foreach ($result as $res){
+//    require 'box.php';
+//}
 
-$query = "SELECT * FROM `user` WHERE `status` = 'false'";
-$result =  select($query);
-foreach ($result as $res){
-    require 'box.php';
+
+$selectedIds = $_POST['selectedIds'];
+
+for($i = 0; $i < count($selectedIds); $i++){
+    $query = "UPDATE `user` SET `status` = 'false' WHERE `user`.`id` = $selectedIds[$i]";
+    execQuery($query);
 }
